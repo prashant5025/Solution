e98a4ceddfef57ec1c40d1ebf382c9a6429d00ac
Prashant Singh <psingh5025615@gmail.com>
2022-10-18 21:17:20 +0530

    initial commit

--- CoffeeHouse.js
+++ CoffeeHouse.js
@@ -1,21 +1,3 @@
-// Question 2:
-// Please implement a software for a Coffee House using Object Oriented Programming. If you are not familiar or
-// comfortable with object-oriented programming, we suggest you give this article a quick read to ensure you
-// understand it before you attempt it.
-// It should have the following features:
-// - Allow users to order a coffee from Espresso, Cappuccino, Latte.
-// - Allow users to specify add-ons: Milk, Cream, Latte
-// Prices for the available options are as follows
-// Product/Add-on Milk Cream Latte
-// Espresso 60 75 100
-// Cappuccino 80 90 125
-// Latte 100 125 150
-// We need to define the solution as follows:
-// 1. Display the combination of coffees that can be ordered
-// 2. Allow users to choose their coffee type with the preferred add-on.
-// 3. Generate the detailed bill/receipt with the amount to be paid.
-// 4. Allow users to order more than 1 cup and more than 1 variety
-
 // Solution:
 
 class Coffee {
